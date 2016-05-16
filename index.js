ee9aacb7049f7d36bd0473f8f489b5d7802ff2cf
Solvek <sa@mobiwolf.com>
2016-05-16 14:34:51 +0300

    Added 10 seconds each time record stopping

--- index.js
+++ index.js
@@ -15,7 +15,41 @@ const ZM_TRIGGER_HOST = 'localhost';
  */
 const ZM_TRIGGER_PORT = 6802;
 
+const MINIMAL_TIME = 10;
+
 var net = require('net');
+
+var stopTimer = null;
+
+var options =  {
+    id: MONITOR_ID,
+    priority: 1,
+    cause: "Alarm",
+    notes: "Motion triggered"};
+
+function sendCommand(start){
+    options.operation = start ? "on" : "off";
+
+    var command = buildZmCommand(options);
+    console.log((new Date()).toString()+": Sending command: "+command);
+    sendData(ZM_TRIGGER_HOST, ZM_TRIGGER_PORT, command+"\n");
+}
+
+function startRecord(message){
+    if (stopTimer){
+        clearTimeout(stopTimer);
+        stopTimer = null;
+    }
+    sendCommand(true);
+}
+
+function stopRecord(){
+    console.log((new Date()).toString()+": Stop initiating");
+    stopTimer = setTimeout(function(){
+        sendCommand(false);
+        stopTimer = null;
+    }, MINIMAL_TIME*1000);
+}
 
 function parseAlarmMessage(data){
     if (data.length < 21){
@@ -69,17 +103,12 @@ var alarmServer = net.createServer(function(socket) {
         }
 
         if (parsed.Event == "MotionDetect"){
-            var options = {
-                id: MONITOR_ID,
-                priority: 1,
-                cause: "Alarm",
-                notes: "Motion triggered"};
-
-            options.operation = (parsed.Status=="Start") ? "on" : "off";
-
-            var command = buildZmCommand(options);
-
-            sendData(ZM_TRIGGER_HOST, ZM_TRIGGER_PORT, command+"\n");
+            if (parsed.Status=="Start"){
+                startRecord(parsed);
+            }
+            else {
+                stopRecord(parsed);
+            }
         }
     });
 });
